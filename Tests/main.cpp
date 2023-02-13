5538e27d3c60e1602315971316d7e5f320787a1b
Dani Camba <Camba3D@gmail.com>
2023-02-13 17:19:32 +0100

    remove messagge level from project cmake

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -2,14 +2,6 @@
 
 int main()
 {
-    // btm::parseGltf("./Assets/Geometry/default_scene_A.gltf");
-    // btm::parseGltf("./Assets/Geometry/default_scene_B.gltf");
-    // auto const a = btm::parseGltf("./Assets/Geometry/default_scene_C.glb");
-
-    // BTM_INFOF("aaaa size : {}", a[0].vertices.size());
-
-    // return 0;
-
     btm::App app { "Bretema", btm::RenderAPI::Vulkan };
     while (true)  // Allows app restart
     {
